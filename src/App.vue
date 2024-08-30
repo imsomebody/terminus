fca011d7b81a78d5d4c6a211c7ca4cc1c8ec8cbd
Davi Viana <davianasales@outlook.com>
2024-08-30 12:10:27 -0300

    fix(App): global focus

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import AppTerminal from './components/AppTerminal.vue';
-import { elements } from './definitions/globals';
+import AppTerminal from "./components/AppTerminal.vue";
+import { elements, getGlobalElement } from "./definitions/globals";
 
-function redirectFocus() {
-  document.getElementById(elements.input)?.focus()
+function redirectFocus(event: FocusEvent | MouseEvent) {
+  if (event.target === getGlobalElement("main")) {
+    document.getElementById(elements.input)?.focus();
+  }
 }
 </script>
 
 <template>
-  <main @focus="redirectFocus" @click="redirectFocus">
+  <main @focus="redirectFocus" @click="redirectFocus" id="app_main">
     <AppTerminal></AppTerminal>
   </main>
 </template>
--- src/definitions/globals.ts
+++ src/definitions/globals.ts
@@ -1,3 +1,8 @@
 export const elements = {
-    input: 'term_input',
-}
+  input: "term_input",
+  main: "app_main",
+};
+
+export const getGlobalElement = (element: keyof typeof elements) => {
+  return document.getElementById(elements[element]);
+};
